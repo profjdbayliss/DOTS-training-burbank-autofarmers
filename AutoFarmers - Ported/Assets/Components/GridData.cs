5d0bccd51b7311cdc140aafd7d52442c35aad267
profjdbayliss <jdbics@rit.edu>
2019-10-31 13:55:38 -0400

    added findtherock
    YES THE ROCK!

--- AutoFarmers - Ported/Assets/Components/GridData.cs
+++ AutoFarmers - Ported/Assets/Components/GridData.cs
@@ -11,6 +11,7 @@ public class GridData : MonoBehaviour
 {
     const int BOARD_MULTIPLIER = 1000; // max board x and y size is 999
     const int ARRAY_MULTIPLIER = 10; // max number of statuses is 9
+    const int ROCK = 1;
 
     public static int sX = 10;
     public static int sZ = 10;
@@ -23,10 +24,19 @@ public class GridData : MonoBehaviour
     {
         gridStatus = new NativeHashMap<int, int>(100, Allocator.Persistent);
 
-        gridStatus.TryAdd(ConvertToHash(4, 4), ConvertDataValue(1, 5));
-        gridStatus.TryAdd(ConvertToHash(8, 8), ConvertDataValue(2, 6));
-        gridStatus.TryAdd(ConvertToHash(15, 15), ConvertDataValue(3, 1));
-        gridStatus.TryAdd(ConvertToHash(4, 4), ConvertDataValue(4, 2));
+        gridStatus.TryAdd(ConvertToHash(5, 4), ConvertDataValue(2, 1));
+        gridStatus.TryAdd(ConvertToHash(8, 5), ConvertDataValue(2, 2));
+        gridStatus.TryAdd(ConvertToHash(7, 4), ConvertDataValue(1, 3));
+        gridStatus.TryAdd(ConvertToHash(5, 15), ConvertDataValue(1, 3));
+        gridStatus.TryAdd(ConvertToHash(8, 15), ConvertDataValue(3, 3));
+
+        // test find the rock
+        //float2 value = FindTheRock(gridStatus, new float2(5, 4), new float2(8, 4), new float2(8, 5), sX, sZ);
+        //Debug.Log("the rock test: " + value.x + " " + value.y);
+
+
+
+        //gridStatus.TryAdd(ConvertToHash(4, 4), ConvertDataValue(4, 2));
         //int temp = 0;
         //gridStatus.TryGetValue(ConvertToHash(7, 7), out temp);
         //float2 tmp = GridData.Search(new float2(7, 7), 5, 3);
@@ -35,11 +45,6 @@ public class GridData : MonoBehaviour
         em = World.Active.EntityManager;
         CreateTestEntity();
     }
-
-    //public void Update()
-    //{
-        
-    //}
 
     public static void InitializeHashMap(int capacity)
     {
@@ -80,6 +85,131 @@ public class GridData : MonoBehaviour
     public static int getCol(int key)
     {
         return key - getRow(key)* BOARD_MULTIPLIER;
+    }
+
+    // assumes good data input for positions and is not checking for positions off the board
+    public static float2 FindTheRock(NativeHashMap<int, int> hashMap, float2 currentPos, float2 middlePos, float2 targetPos, int sizeX, int sizeZ)
+    {
+        int startX = (int)currentPos.x;
+        int startY = (int)currentPos.y;
+        int endX = (int)middlePos.x;
+        int endY = (int)middlePos.y;
+
+        int i = 0;
+        int j = 0;
+        int countEnd = 0;
+        int value = 0;
+        if (endX-startX != 0)
+        {
+            // this is the dir we're searching
+            if (endX < startX)
+            {
+                i = endX;
+                countEnd = startX;
+            }
+            else
+            {
+                i = startX;
+                countEnd = endX;
+            }
+            j = startY;
+            for (; i < countEnd; i++)
+            { 
+                    hashMap.TryGetValue(GridData.ConvertToHash(i, j), out value);
+                    if (getStatus(value) == ROCK)
+                    {
+                        return new float2(i, j);
+                    }
+            }
+
+        } else
+        {
+            // this is the dir we're searching
+            if (endY < startY)
+            {
+                j = endY;
+                countEnd = startY;
+            }
+            else
+            {
+                j = startY;
+                countEnd = endY;
+            }
+            i = startX;
+            for (; i < countEnd; i++)
+            {
+                hashMap.TryGetValue(GridData.ConvertToHash(i, j), out value);
+                if (getStatus(value) == ROCK)
+                {
+                    return new float2(i, j);
+                }
+            }
+        }
+
+
+
+        // no rocks on path to middle, so try path from middle to end
+        startX = (int)middlePos.x;
+        startY = (int)middlePos.y;
+        endX = (int)targetPos.x;
+        endY = (int)targetPos.y;
+       
+        i = 0;
+        j = 0;
+        countEnd = 0;
+        value = 0;
+        if (endX - startX != 0)
+        {
+            // this is the dir we're searching
+            if (endX < startX)
+            {
+                i = endX;
+                countEnd = startX;
+            }
+            else
+            {
+                i = startX;
+                countEnd = endX;
+            }
+            j = startY;
+            for (; i < countEnd; i++)
+            {
+                hashMap.TryGetValue(GridData.ConvertToHash(i, j), out value);
+                if (getStatus(value) == ROCK)
+                {
+                    return new float2(i, j);
+                }
+            }
+
+        }
+        else
+        {
+            // this is the dir we're searching
+            if (endY < startY)
+            {
+                j = endY;
+                countEnd = startY;
+            }
+            else
+            {
+                j = startY;
+                countEnd = endY;
+            }
+            i = startX;
+            for (; i < countEnd; i++)
+            {
+                hashMap.TryGetValue(GridData.ConvertToHash(i, j), out value);
+                if (getStatus(value) == ROCK)
+                {
+                    return new float2(i, j);
+                }
+            }
+        }
+
+
+
+        // no rocks means this return
+        return new float2(-1, -1);
     }
 
     public static float2 Search(NativeHashMap<int, int> hashMap, float2 currentPos, int radius, int statusToFind, int sizeX, int sizeZ)
